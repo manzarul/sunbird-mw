42d5aecf13435b473d8c2617e6a0e0b723ee65bd
amit-tarento <amit.kumar@tarento.com>
2017-05-05 17:54:39 +0530

    code change for testing

--- micro-services/learner-state-service/app/controllers/LearnerController.java
+++ micro-services/learner-state-service/app/controllers/LearnerController.java
@@ -4,6 +4,15 @@ import play.libs.F.Promise;
 import play.mvc.Result;
 import play.mvc.Results;
 import org.sunbird.learner.actors.*;
+import org.sunbird.model.Course;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import com.typesafe.config.ConfigFactory;
+import org.sunbird.bean.*;
+import org.sunbird.model.*;
 /**
  * This controller will handler all the request related 
  * to learner state.
@@ -19,6 +28,25 @@ public class LearnerController extends BaseController {
 	 */
 	public Result getEnrolledCourses() {
 		LearnerStateActor actor = null;
+		ActorSystem system =null;
+		system = ActorSystem.create("HelloApplication", ConfigFactory.load()
+                .getConfig("HelloConfig"));
+		ActorSelection selection = system.actorSelection("akka.tcp://WorldApplication@127.0.0.1:8088/user/WorldActor");
+		
+		Course course= new Course();
+    	course.setCourseId("Course 12");
+    	course.setCourseName("Course Name");
+    	course.setUserId("user ID 1");
+    	course.setEnrolledDate("2017-05-05");
+    	course.setDescription("Teacher training Course Material");
+    	course.setCourseProgressStatus("Not Started");
+    	course.setActive(true);
+    	
+		ActorMessage msg = new ActorMessage();
+		msg.setOperation(LearnerStateOperation.ADD_COURSE);
+		Map map = new HashMap<String,Object>();
+		map.put("Course 1",course);
+		selection.tell(msg, ActorRef.noSender());
 		return ok("success");
 	}
 	
