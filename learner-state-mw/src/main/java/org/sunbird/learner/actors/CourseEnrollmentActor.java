c60dd1450f2bfde3bf2b70a1e523f77b4d158a9e
arvindyadav108 <arvind.glaitm108@gmail.com>
2017-05-05 17:52:41 +0530

    add course with akka

--- learner-state-mw/src/main/java/org/sunbird/learner/actors/CourseEnrollmentActor.java
+++ learner-state-mw/src/main/java/org/sunbird/learner/actors/CourseEnrollmentActor.java
@@ -6,7 +6,11 @@ package org.sunbird.learner.actors;
 import org.sunbird.bean.ActorMessage;
 
 import akka.actor.UntypedActor;
+import org.sunbird.bean.LearnerStateOperation;
+import org.sunbird.cassandra.CassandraOperation;
+import org.sunbird.cassandraImpl.CassandraOperationImpl;
 import org.sunbird.common.*;
+import org.sunbird.model.Course;
 
 /**
  * This class will handle course enrollment
@@ -15,17 +19,24 @@ import org.sunbird.common.*;
  *
  */
 public class CourseEnrollmentActor  extends UntypedActor{
-     private CassandraUtil cassandraUtil;
+
+	private CassandraOperation cassandraOperation = new CassandraOperationImpl();
 	@Override
 	public void onReceive(Object message) throws Throwable {
 		if(message instanceof ActorMessage) {
 			//TODO check the operation type and handle it.
-			// call to cassndra for the course enrollmant
-			//cassandraUtil.
+			ActorMessage actorMessage = (ActorMessage)message;
 
-
+			if(actorMessage.getOperation().getValue().equalsIgnoreCase(LearnerStateOperation.ADD_COURSE.getValue())){
+				Object obj = actorMessage.getData().keySet().toArray()[0];
+				if(obj instanceof Course) {
+					Course course = (Course) obj;
+					cassandraOperation.insertCourse(course);
+					sender().tell("SUCCESS", self());
+				}else{
+					sender().tell("UNSUPPORTED COURSE OBJECT",self());
+				}
+			}
 		}
-		
 	}
-
 }
