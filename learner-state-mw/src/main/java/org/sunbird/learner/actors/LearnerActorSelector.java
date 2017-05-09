46a90b4d9e7cf0c6b5947cee848816fba81dacc6
arvindyadav108 <arvind.glaitm108@gmail.com>
2017-05-09 14:25:40 +0530

    making child actor call async

--- learner-state-mw/src/main/java/org/sunbird/learner/actors/LearnerActorSelector.java
+++ learner-state-mw/src/main/java/org/sunbird/learner/actors/LearnerActorSelector.java
@@ -3,10 +3,16 @@ package org.sunbird.learner.actors;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.actor.UntypedAbstractActor;
+import akka.dispatch.OnComplete;
+import akka.pattern.Patterns;
 import akka.routing.FromConfig;
+import akka.util.Timeout;
 import org.apache.log4j.Logger;
 import org.sunbird.bean.ActorMessage;
 import org.sunbird.bean.LearnerStateOperation;
+import scala.concurrent.ExecutionContext;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
 
 /**
  * Created by arvind on 8/5/17.
@@ -18,8 +24,10 @@ public class LearnerActorSelector extends UntypedAbstractActor {
     ActorRef courseEnrollmentActorRouter;
     ActorRef learnerStateActorRouter;
     ActorRef learnerStateUpdateActorRouter;
+    private ExecutionContext ec;
 
-    public LearnerActorSelector(){
+    // constructor to initialize router actor with child actor pool
+    public LearnerActorSelector() {
         courseEnrollmentActorRouter =
                 getContext().actorOf(FromConfig.getInstance().props(Props.create(CourseEnrollmentActor.class)),
                         "router1");
@@ -29,6 +37,7 @@ public class LearnerActorSelector extends UntypedAbstractActor {
         learnerStateUpdateActorRouter =
                 getContext().actorOf(FromConfig.getInstance().props(Props.create(LearnerStateUpdateActor.class)),
                         "router3");
+        ec = getContext().dispatcher();
     }
 
     @Override
@@ -40,15 +49,63 @@ public class LearnerActorSelector extends UntypedAbstractActor {
             ActorMessage actorMessage = (ActorMessage) message;
             if (actorMessage.getOperation().getValue().equalsIgnoreCase(LearnerStateOperation.ADD_COURSE.getValue())) {
 
-                courseEnrollmentActorRouter.tell(message, self());
+                Timeout timeout = new Timeout(Duration.create(5, "seconds"));
+                Future<Object> future = Patterns.ask(courseEnrollmentActorRouter, message, timeout);
+
+                future.onComplete(new OnComplete<Object>() {
+                    @Override
+                    public void onComplete(Throwable failure, Object result) {
+                        if (failure != null) {
+                            //We got a failure, handle it here
+                        } else {
+                            logger.info("PARENT RESULT IS ");
+                            // We got a result, do something with it
+                            sender().tell("SUCCESS", ActorRef.noSender());
+                        }
+                    }
+                }, ec);
+
+                //courseEnrollmentActorRouter.tell(message, self());
 
             } else if (actorMessage.getOperation().getValue().equalsIgnoreCase(LearnerStateOperation.GET_COURSE.getValue())) {
 
-                learnerStateActorRouter.tell(message, self());
+                Timeout timeout = new Timeout(Duration.create(5, "seconds"));
+                Future<Object> future = Patterns.ask(learnerStateActorRouter, message, timeout);
+
+                future.onComplete(new OnComplete<Object>() {
+                    @Override
+                    public void onComplete(Throwable failure, Object result) {
+                        if (failure != null) {
+                            //We got a failure, handle it here
+                        } else {
+                            logger.info("PARENT RESULT IS ");
+                            // We got a result, do something with it
+                            sender().tell("SUCCESS", ActorRef.noSender());
+                        }
+                    }
+                }, ec);
+
+                //learnerStateActorRouter.tell(message, self());
 
             } else if (actorMessage.getOperation().getValue().equalsIgnoreCase(LearnerStateOperation.UPDATE_TOC.getValue())) {
 
-                learnerStateUpdateActorRouter.tell(message, self());
+                Timeout timeout = new Timeout(Duration.create(5, "seconds"));
+                Future<Object> future = Patterns.ask(learnerStateUpdateActorRouter, message, timeout);
+
+                future.onComplete(new OnComplete<Object>() {
+                    @Override
+                    public void onComplete(Throwable failure, Object result) {
+                        if (failure != null) {
+                            //We got a failure, handle it here
+                        } else {
+                            logger.info("PARENT RESULT IS ");
+                            // We got a result, do something with it
+                            sender().tell("SUCCESS", ActorRef.noSender());
+                        }
+                    }
+                }, ec);
+
+                //learnerStateUpdateActorRouter.tell(message, self());
 
             }
 
--- learner-state-mw/src/main/java/org/sunbird/learner/actors/LearnerStateActor.java
+++ learner-state-mw/src/main/java/org/sunbird/learner/actors/LearnerStateActor.java
@@ -1,28 +1,58 @@
 package org.sunbird.learner.actors;
 
 
+import akka.actor.UntypedAbstractActor;
+import org.apache.log4j.Logger;
 import org.sunbird.bean.ActorMessage;
+import org.sunbird.bean.LearnerStateOperation;
 import org.sunbird.cassandra.CassandraOperation;
 import org.sunbird.cassandraImpl.CassandraOperationImpl;
 
-import akka.actor.UntypedAbstractActor;
-
 /**
  * This actor will provide learner TOC state.
- * @author Manzarul
  *
+ * @author Manzarul
  */
-public class LearnerStateActor extends UntypedAbstractActor{
+public class LearnerStateActor extends UntypedAbstractActor {
 
-	private CassandraOperation cassandraOperation = new CassandraOperationImpl();
+    private CassandraOperation cassandraOperation = new CassandraOperationImpl();
+    Logger logger = Logger.getLogger(LearnerStateActor.class.getName());
 
-	@Override
+    @Override
     public void onReceive(Object message) throws Exception {
-		if(message instanceof ActorMessage) {
-			//TODO
+        if (message instanceof ActorMessage) {
+            logger.info("onReceive called");
+            ActorMessage actorMessage = (ActorMessage) message;
+            if (actorMessage.getOperation().getValue().equalsIgnoreCase(LearnerStateOperation.GET_COURSE.getValue())) {
+                logger.info("OP type match" + actorMessage.getData().size());
+                Object obj = actorMessage.getData().get(actorMessage.getData().keySet().toArray()[0]);
+                if (obj instanceof String) {
+                    logger.info("Obj match");
+                    String userId = (String) obj;
+                    //TODO: add course by user id at Cassandra-Operation
+                    //cassandraOperation.get
+                    cassandraOperation.getUserEnrolledCourse(userId);
+                    sender().tell("SUCCESS", self());
+                } else {
+                    logger.info("Mis match");
+                    sender().tell("UNSUPPORTED COURSE OBJECT", self());
+                }
+            } else if (actorMessage.getOperation().getValue().equalsIgnoreCase(LearnerStateOperation.GET_COURSE_BY_ID.getValue())) {
+                logger.info("OP type match" + actorMessage.getData().size());
+                Object obj = actorMessage.getData().get(actorMessage.getData().keySet().toArray()[0]);
+                if (obj instanceof String) {
+                    logger.info("Obj match");
+                    String courseId = (String) obj;
+                    cassandraOperation.getCourseById(courseId);
+                    sender().tell("SUCCESS", getSelf());
+                } else {
+                    logger.info("Mis match");
+                    sender().tell("UNSUPPORTED COURSE OBJECT", self());
+                }
+            }
 
-		}
-	  
+        }
+
     }
 
 }
