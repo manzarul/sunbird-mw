0833229006c220eb9a8f38a50d59e96cc05e8454
manzarul.haque <manzarul.haque>
2017-05-11 12:21:39 +0530

    adding for logger and comments

--- sunbird-common/common/src/main/java/org/sunbird/common/models/response/Response.java
+++ sunbird-common/common/src/main/java/org/sunbird/common/models/response/Response.java
@@ -8,9 +8,10 @@ import org.sunbird.common.responsecode.HeaderResponseCode;
 
 
 /**
- * 
- * @author rayulu
- * 
+ * This is a common response class for all the layer.
+ * All layer will send same response object.
+ * @author Manzarul
+ *
  */
 public class Response implements Serializable {
 
@@ -22,27 +23,46 @@ public class Response implements Serializable {
     private ResponseParams params;
     private HeaderResponseCode responseCode = HeaderResponseCode.OK;
     private Map<String, Object> result = new HashMap<String, Object>();
-
+    
+    /**
+     * This will provide request unique id.
+     * @return String
+     */
     public String getId() {
         return id;
     }
-
+    /**
+     * set the unique id
+     * @param id String
+     */
     public void setId(String id) {
         this.id = id;
     }
-
+    /**
+     * this will provide api version
+     * @return String
+     */
     public String getVer() {
         return ver;
     }
-
+    /**
+     * set the api version
+     * @param ver String
+     */
     public void setVer(String ver) {
         this.ver = ver;
     }
-
+    /**
+     * this will provide complete time value
+     * @return String
+     */
     public String getTs() {
         return ts;
     }
-
+    /**
+     * set the time value
+     * @param ts String
+     */
     public void setTs(String ts) {
         this.ts = ts;
     }
@@ -53,27 +73,47 @@ public class Response implements Serializable {
     public Map<String, Object> getResult() {
         return result;
     }
-
+   /**
+    * 
+    * @param key
+    * @return
+    */
     public Object get(String key) {
         return result.get(key);
     }
-
+   /**
+    * 
+    * @param key
+    * @param vo
+    */
     public void put(String key, Object vo) {
         result.put(key, vo);
     }
-
+    /**
+     * This will provide response parameter object.
+     * @return ResponseParams
+     */
     public ResponseParams getParams() {
         return params;
     }
-
+   /**
+    * set the response parameter object.
+    * @param params
+    */
     public void setParams(ResponseParams params) {
         this.params = params;
     }
-
+    /**
+     * Set the response code for header. 
+     * @param code  HeaderResponseCode
+     */
     public void setResponseCode(HeaderResponseCode code) {
         this.responseCode = code;
     }
-
+    /**
+     * get the response code
+     * @return HeaderResponseCode
+     */
     public HeaderResponseCode getResponseCode() {
         return this.responseCode;
     }
--- sunbird-common/common/src/main/java/org/sunbird/common/models/response/ResponseParams.java
+++ sunbird-common/common/src/main/java/org/sunbird/common/models/response/ResponseParams.java
@@ -3,9 +3,9 @@ package org.sunbird.common.models.response;
 import java.io.Serializable;
 
 /**
- * 
- * @author rayulu
- * 
+ * This class will contains response  envelop.
+ * @author Manzarul
+ *
  */
 public class ResponseParams implements Serializable {
 
@@ -16,48 +16,78 @@ public class ResponseParams implements Serializable {
     private String err;
     private String status;
     private String errmsg;
-
+    
     public enum StatusType {
 
         successful, warning, failed;
     }
-
+    /**
+     * This will contains response message id.
+     * @return String
+     */
     public String getResmsgid() {
         return resmsgid;
     }
-
+    /**
+     * set the response message id.
+     * @param resmsgid String
+     */
     public void setResmsgid(String resmsgid) {
         this.resmsgid = resmsgid;
     }
-
+    /**
+     * This will provide request specific message id.
+     * @return String
+     */
     public String getMsgid() {
         return msgid;
     }
-
+    /**
+     * Set the request specific message id.
+     * @param msgid
+     */
     public void setMsgid(String msgid) {
         this.msgid = msgid;
     }
-
+    /**
+     * This will provide error message
+     * @return String
+     */
     public String getErr() {
         return err;
     }
-
+    /**
+     * Set the error message
+     * @param err String
+     */
     public void setErr(String err) {
         this.err = err;
     }
-
+    /**
+     * This will return api call status
+     * @return String
+     */
     public String getStatus() {
         return status;
     }
-
+    /**
+     * Set the api call status
+     * @param status
+     */
     public void setStatus(String status) {
         this.status = status;
     }
-
+   /**
+    * This will provide Error message in english
+    * @return String
+    */
     public String getErrmsg() {
         return errmsg;
     }
-
+    /**
+     * Set the error message in English.
+     * @param message String
+     */
     public void setErrmsg(String message) {
         this.errmsg = message;
     }
@@ -66,12 +96,4 @@ public class ResponseParams implements Serializable {
     public String toString() {
         return "Params [" + (resmsgid !=null ? "resmsgid=" + resmsgid + ", " : "") + (msgid != null ? "msgid=" + msgid + ", " : "") + (err != null ? "err=" + err + ", " : "") + (status != null ? "status=" + status + ", " : "") + (errmsg != null ? "errmsg=" + errmsg :"") + "]";
     }
-
-    // @Override
-    // public String toString() {
-    // return "Params [" + (err != null ? "err=" + err + ", " : "") + (status !=
-    // null ? "statusType=" + status + ", " : "")
-    // + (errmsg != null ? "errmsg=" + errmsg + ", " : "") + "]";
-    // }
-
 }
